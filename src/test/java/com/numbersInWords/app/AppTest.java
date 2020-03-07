cb723cbe6d37500e43cf252e4b0b51b69a57a224
aluno <aluno@BIBLIOTECA09>
2020-03-07 19:56:15 -0300

    commit

---
+++ src/test/java/com/numbersInWords/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.numbersInWords.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
